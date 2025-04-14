4d42d7bda6a0b3b8c9c5e99d737f48f131bcce6d
Tung Le <lenhattung@gmail.com>
2025-04-14 09:56:38 +0700

    ud
    ud

--- Slot02_a/src/slot02_a/Slot02_a.java
+++ Slot02_a/src/slot02_a/Slot02_a.java
@@ -21,16 +21,24 @@ public class Slot02_a {
         ArrayList<Integer> list1 = new ArrayList<Integer>();
         list1.add(5);
         list1.add(10);
-        for (int i = 0; i < 100000; i++) {
+        for (int i = 0; i < 10; i++) {
             list1.add(i);
         }
         for (int i = 0; i < 10; i++) {
             System.out.println(list1.get(i));
         }
         
-        list1.remove(0);
+//        list1.remove(0);
+//        
+//        list1.remove(list1.size()-1);
         
-        list1.remove(list1.size()-1);
+        list1.add(0, -1);
+        
+        list1.add(list1.size()-1, -100);
+        
+        list1.indexOf(5); // Vị trí
+        
+        list1.get(list1.indexOf(5));
         
         // list1....
 
