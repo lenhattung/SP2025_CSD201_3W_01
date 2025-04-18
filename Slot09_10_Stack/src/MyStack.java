fb9e02f65729af3a0d90890d3a2ee63b45e36ed3
Tung Le <lenhattung@gmail.com>
2025-04-18 09:43:26 +0700

    ud
    ud

--- Slot09_10_Stack/src/MyStack.java
+++ Slot09_10_Stack/src/MyStack.java
@@ -11,10 +11,8 @@ public class MyStack {
     public void push(int x) {
         // Create new node with given data
         Node newNode = new Node(x);
-
         // Make the new node point to the current top
         newNode.next = top;
-
         // Update top to point to the new node
         top = newNode;
     }
@@ -28,5 +26,34 @@ public class MyStack {
         this.push(2);
         this.push(15);
         this.push(9);
+    }
+
+    // Utility function to check if the stack is empty or not
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+    // function to return value of top element in a stack
+    // -1: return a default value if stack is empty
+    public int peek() {
+        if (!isEmpty()) {
+            return top.info;
+        } else {
+            System.out.println("Stack is empty!");
+            return -1;
+        }
+    }
+
+    // function to return value of top element and remove top element in a stack
+    // -1: return a default value if stack is empty
+    public int pop() {
+        if (!isEmpty()) {
+            int value = top.info; // lưu giữ giá trị của top
+            top = top.next; // dời top sang phần tử tiếp theo
+            return value; // trả về giá trị của top
+        } else {
+            System.out.println("Stack is empty!");
+            return -1;
+        }
     }
 }
